0a7d4030ef6923e120e110a16ffbae1379b374ab
Lucas Borges <lucas.pborges@yahoo.com.br>
2022-01-26 18:49:17 -0300

    criando uma toast personalizada

--- app/src/main/java/br/com/borges/lucas/elementsapplication/MainActivity.kt
+++ app/src/main/java/br/com/borges/lucas/elementsapplication/MainActivity.kt
@@ -1,8 +1,10 @@
 package br.com.borges.lucas.elementsapplication
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.Gravity
+import android.view.LayoutInflater
 import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
 import br.com.borges.lucas.elementsapplication.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
@@ -16,6 +18,17 @@ class MainActivity : AppCompatActivity() {
 
     binding.buttonToast.setOnClickListener{
       val toast = Toast.makeText( this, "Toast", Toast.LENGTH_LONG )
+
+      //pegando o texto de dentro do toast
+//      val text = toast.view?.findViewById<TextView>( R.id.message )
+//      text?.setTextColor( Color.RED )
+
+      //setando um layout personalizado para a toast
+      val layout = LayoutInflater.from(this).inflate( R.layout.toast_layout, null )
+      toast.view = layout
+
+      //setando o gravity do toast
+      toast.setGravity( Gravity.TOP, 0, 0 )
       toast.show()
     }
   }
