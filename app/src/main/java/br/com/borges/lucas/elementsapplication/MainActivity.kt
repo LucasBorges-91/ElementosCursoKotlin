f36899eee7c5e55726386cb0061103a2dd4eb8f6
Lucas Borges <lucas.pborges@yahoo.com.br>
2022-01-27 14:56:18 -0300

    adicionando switch

--- app/src/main/java/br/com/borges/lucas/elementsapplication/MainActivity.kt
+++ app/src/main/java/br/com/borges/lucas/elementsapplication/MainActivity.kt
@@ -6,14 +6,13 @@ import android.view.Gravity
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewParent
-import android.widget.AdapterView
-import android.widget.ArrayAdapter
-import android.widget.Toast
+import android.widget.*
 import androidx.appcompat.app.AppCompatActivity
 import br.com.borges.lucas.elementsapplication.databinding.ActivityMainBinding
 import com.google.android.material.snackbar.Snackbar
 
-class MainActivity : AppCompatActivity(), AdapterView.OnItemSelectedListener {
+class MainActivity : AppCompatActivity(), AdapterView.OnItemSelectedListener,
+  SeekBar.OnSeekBarChangeListener, CompoundButton.OnCheckedChangeListener {
   private lateinit var binding: ActivityMainBinding
 
   override fun onCreate(savedInstanceState: Bundle?) {
@@ -22,30 +21,30 @@ class MainActivity : AppCompatActivity(), AdapterView.OnItemSelectedListener {
     binding = ActivityMainBinding.inflate(layoutInflater)
     setContentView(binding.root)
 
-    binding.buttonToast.setOnClickListener{
-      val toast = Toast.makeText( this, "Toast", Toast.LENGTH_LONG )
+    binding.buttonToast.setOnClickListener {
+      val toast = Toast.makeText(this, "Toast", Toast.LENGTH_LONG)
 
       //pegando o texto de dentro do toast
 //      val text = toast.view?.findViewById<TextView>( R.id.message )
 //      text?.setTextColor( Color.RED )
 
       //setando um layout personalizado para a toast
-      val layout = LayoutInflater.from(this).inflate( R.layout.toast_layout, null )
+      val layout = LayoutInflater.from(this).inflate(R.layout.toast_layout, null)
       toast.view = layout
 
       //setando o gravity do toast
-      toast.setGravity( Gravity.TOP, 0, 0 )
+      toast.setGravity(Gravity.TOP, 0, 0)
       toast.show()
     }
 
-    binding.buttonSnack.setOnClickListener{
-      val snack = Snackbar.make(binding.linearRoot, "Snack", Snackbar.LENGTH_SHORT )
+    binding.buttonSnack.setOnClickListener {
+      val snack = Snackbar.make(binding.linearRoot, "Snack", Snackbar.LENGTH_SHORT)
 
-      snack.setAction( "desfazer" , View.OnClickListener {
-        toast( "desfeito" )
+      snack.setAction("desfazer", View.OnClickListener {
+        toast("desfeito")
       })
-      snack.setActionTextColor( Color.BLUE )
-      snack.setBackgroundTint( Color.GRAY )
+      snack.setActionTextColor(Color.BLUE)
+      snack.setBackgroundTint(Color.GRAY)
       snack.show()
     }
 
@@ -54,38 +53,71 @@ class MainActivity : AppCompatActivity(), AdapterView.OnItemSelectedListener {
     //configura spinner dinamico
     loadSpinner()
 
-    binding.buttonGetSpinner.setOnClickListener{
+    binding.buttonGetSpinner.setOnClickListener {
       val selectItem = binding.spinnerStatic.selectedItem
       val selectItemId = binding.spinnerStatic.selectedItemId
       val selectItemPosition = binding.spinnerStatic.selectedItemPosition
 
-      toast( "position: $selectItemId: $selectItem")
+      toast("position: $selectItemId: $selectItem")
     }
 
-    binding.buttonSetSpinner.setOnClickListener{
+    binding.buttonSetSpinner.setOnClickListener {
       binding.spinnerStatic.setSelection(2)
     }
+
+    binding.seekbar.setOnSeekBarChangeListener(this)
+
+    binding.buttonGetSeek.setOnClickListener {
+      toast( "Seekbar: ${binding.seekbar.progress}")
+    }
+
+    binding.buttonSetSeek.setOnClickListener {
+      binding.seekbar.progress = 10
+    }
+
+    binding.switchButton.setOnCheckedChangeListener( this )
   }
 
   private fun loadSpinner() {
     val mList = listOf("gramas", "kg", "arroba", "tonelada")
-    val adapter = ArrayAdapter( this, android.R.layout.simple_spinner_dropdown_item, mList )
+    val adapter = ArrayAdapter(this, android.R.layout.simple_spinner_dropdown_item, mList)
     binding.spinnerDynamic.adapter = adapter
   }
 
-  private fun toast( str: String ) {
-    Toast.makeText( this, str, Toast.LENGTH_LONG ).show()
+  private fun toast(str: String) {
+    Toast.makeText(this, str, Toast.LENGTH_LONG).show()
   }
 
   override fun onItemSelected(parent: AdapterView<*>?, p1: View?, position: Int, p3: Long) {
-    when ( parent?.id ) {
+    when (parent?.id) {
       R.id.spinner_static -> {
-        toast( parent?.getItemAtPosition( position ).toString() )
+        toast(parent?.getItemAtPosition(position).toString())
       }
     }
   }
 
   override fun onNothingSelected(p0: AdapterView<*>?) {
-    toast( "nothing" )
+    toast("nothing")
+  }
+
+  override fun onProgressChanged(p0: SeekBar?, progress: Int, p2: Boolean) {
+    binding.textSeekbarValue.text = "valor seekbar: $progress"
+  }
+
+  override fun onStartTrackingTouch(p0: SeekBar?) {
+    toast( "track started")
+  }
+
+  override fun onStopTrackingTouch(p0: SeekBar?) {
+    toast( "track stoped")
+  }
+
+  override fun onCheckedChanged(p0: CompoundButton?, p1: Boolean) {
+    when ( p0?.id ) {
+      R.id.switch_button -> {
+//        binding.switchButton.isChecked = true
+        toast( "Switch: ${if (p1) "true" else "false"}")
+      }
+    }
   }
 }
